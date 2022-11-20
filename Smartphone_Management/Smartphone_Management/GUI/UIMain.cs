c7b28d59bcbf0314dcf6a9c68322557da780d896
Trần Quốc Vương <tranquocvuong0603@gmail.com>
2022-11-20 11:26:40 +0700

    add

--- Smartphone_Management/Smartphone_Management/GUI/UIMain.cs
+++ Smartphone_Management/Smartphone_Management/GUI/UIMain.cs
@@ -20,6 +20,9 @@ namespace Smartphone_Management
     {
         private IconButton currentBtn;
 
+        private thongTinPhieuNhap form1;
+        private themPhieuNhap form2;
+
         private Color corlor = Color.FromArgb(0, 127, 0);
         private int FlagColor = 0;
         public UIMain()
@@ -200,12 +203,14 @@ namespace Smartphone_Management
 
         private void btnTaoPhieuNhap_Click(object sender, EventArgs e)
         {
-
+            form2 = new themPhieuNhap();
+            form2.Show();
         }
 
         private void btnTTPhieuNhap_Click(object sender, EventArgs e)
         {
-
+            form1 = new thongTinPhieuNhap();
+            form1.Show();
         }
 
         private void btnChinhSachKhuyenMai_Click(object sender, EventArgs e)
@@ -218,9 +223,9 @@ namespace Smartphone_Management
 
         }
 
+        private void UIMain_Load(object sender, EventArgs e)
+        {
 
-
-
-      
+        }
     }
 }
